9204ffb0fc622b89abe068c0b13244d4e67c93a4
mengxy@asiainfo.com <mengxy@asiainfo.com>
2018-06-10 11:14:04 +0800

    模型数据从表单向controller传递数据

--- src/main/java/com/A.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-/**
- * Created by Mengxy on 2018/6/4.
- */
-public class A {
-}
--- src/main/java/com/qfedu/B.java
+++
@@ -1,7 +0,0 @@
-package com.qfedu;
-
-/**
- * Created by Mengxy on 2018/6/4.
- */
-public class B {
-}
